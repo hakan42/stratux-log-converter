25b61c58680053404448a313e1e8667488b91c09
Hakan Tandogan <hakan@tandogan.com>
2025-09-26 13:07:44 +0000

    Inject tail number
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- run.sh
+++ run.sh
@@ -4,16 +4,15 @@ IN=/var/log
 [ $(uname -n) = "wallaby" ] && IN=../var-log-from-pi
 
 OUT=../converted
-mkdir -p ${OUT}
+
+TAIL_NUMBER=${1:-DEABC}
 
 for f in ${IN}/sensors_*.csv
 do
     i=$(basename $f)
-    # o=$(echo $i | sed -e 's|.csv$|.gpx|')
-    o=$(echo $i | sed -e 's|sensors_|stratux_converted_ff_|')
-    echo $f $i $o
 
-    ./stratux_sensors_to_foreflight.py $f -o ${OUT}/$o --airports airports.csv --debug
+    o=${OUT}/foreflight/$(echo $i | sed -e 's|sensors_|stratux_converted_ff_|')
+    # echo $f $i $o
+    mkdir -p $(dirname $o)
+    ./stratux_sensors_to_foreflight.py $f -o $o --tail-number=${TAIL_NUMBER} --airports airports.csv --debug
 done
-
-
